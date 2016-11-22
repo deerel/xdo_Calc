5b8fb63a6e3664d2c385e3bff41c8644c309893a
deerel <deerel@gmail.com>
2016-11-22 15:23:03 +0100

    Dynamic font size on display. Cleaning up the code.

--- app/src/main/java/se/xdo/prelab01/MainActivity.java
+++ app/src/main/java/se/xdo/prelab01/MainActivity.java
@@ -3,7 +3,6 @@ package se.xdo.prelab01;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,11 +12,12 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int[] numericButtons = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
-    private int[] operatorButtons = {R.id.buttonPlus, R.id.buttonMinus, R.id.buttonTimes, R.id.buttonDivide};
-    private TextView txtScreen;
+    //Setting up array with all buttons
+    private int[] numButtons = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
+    private int[] opButtons = {R.id.buttonPlus, R.id.buttonMinus, R.id.buttonTimes, R.id.buttonDivide};
+    private TextView sumDisplay;
     private String leftNum, rightNum, operand;
-    private boolean atStart = true;
+    private boolean atStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +25,11 @@ public class MainActivity extends AppCompatActivity {
         //Force app to portrait
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
-        this.txtScreen = (TextView) findViewById(R.id.textView);
+        sumDisplay = (TextView) findViewById(R.id.textView);
+        fixFontSize();
         setNumericOnClickListener();
         setOperatorOnClickListener();
-        leftNum = "";
-        rightNum = "";
-        operand = "";
+        clearAll();
     }
 
     private void setNumericOnClickListener() {
@@ -38,23 +37,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Button button = (Button) v;
+                //If operand is given or not
                 if("".equals(operand)) {
+                    //If digit is given or not
                     if(atStart) {
                         leftNum = button.getText().toString();
                     } else {
                         leftNum = leftNum + button.getText().toString();
                     }
-                    txtScreen.setText(leftNum);
+                    sumDisplay.setText(leftNum);
                     atStart=false;
                 } else {
                     rightNum = rightNum + button.getText().toString();
-                    txtScreen.setText(rightNum);
+                    sumDisplay.setText(rightNum);
                 }
+
+                fixFontSize();
             }
         };
 
-        for (int id : numericButtons) {
-            findViewById(id).setOnClickListener(listener);
+        for (int id : numButtons) {
+                findViewById(id).setOnClickListener(listener);
         }
     }
 
@@ -63,7 +66,8 @@ public class MainActivity extends AppCompatActivity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if("".equals(rightNum)) {
+                //Execute expression evaluation if there are a right number
+                if(!"".equals(rightNum)) {
                     onEqual(false);
                 }
                 Button button = (Button) v;
@@ -71,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        for (int id : operatorButtons) {
+        for (int id : opButtons) {
             findViewById(id).setOnClickListener(listener);
         }
 
@@ -83,45 +87,59 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*
-    private void opEqual(Boolean equal) {
-        if("".equals(leftNum) || "".equals(rightNum) || "".equals(operand)) {
+    private void onEqual(Boolean equal) {
+        //If equals is pressed and expression not is complete, do nothing.
+        if(!isCompleteExpression()) {
             return;
         }
         String txt = leftNum + operand +  rightNum;
         Expression expression = new ExpressionBuilder(txt).build();
-        double result = expression.evaluate();
-        txtScreen.setText(Double.toString(result));
-        leftNum = Double.toString(result);
+        String sumText = "";
+        double result = 0;
+        try {
+            result = expression.evaluate();
+            sumText = Double.toString(result);
+            sumDisplay.setText(sumText);
+            leftNum = sumText;
+        } catch (Exception e) {
+            clearAll();
+            sumDisplay.setText(R.string.error);
+        }
+
+        fixFontSize();
+        clear(equal);
+
+
+    }
+
+    private boolean isCompleteExpression() {
+        if("".equals(leftNum) || "".equals(rightNum) || "".equals(operand)) {
+            return false;
+        }
+        return true;
+    }
+
+    private void fixFontSize() {
+        if(sumDisplay.length()>11 && sumDisplay.length()<=20) {
+            sumDisplay.setTextSize(44-(sumDisplay.length()-11)*2.2f);
+        } else {
+            sumDisplay.setTextSize(44);
+        }
+    }
+
+    private void clearAll() {
+        leftNum = "0";
         rightNum = "";
+        operand = "";
         atStart = true;
     }
-    */
 
-    private void onEqual(Boolean equal) {
-        if("".equals(leftNum) || "".equals(rightNum) || "".equals(operand)) {
-            return;
-        }
-
-        String txt = leftNum + operand +  rightNum;
-        Expression expression = new ExpressionBuilder(txt).build();
-        try {
-            double result = expression.evaluate();
-            txtScreen.setText(Double.toString(result));
-            leftNum = Double.toString(result);
-        } catch (Exception e) {
-            leftNum = "";
-            rightNum = "";
-            operand = "";
-            atStart = true;
-            txtScreen.setText(R.string.error);
-        }
-
+    private void clear(Boolean equal) {
         rightNum = "";
         atStart = true;
+
         if(equal) {
             operand = "";
         }
-
     }
 }
